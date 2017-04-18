373db91c684a5b34d4456d59790743f155796295
aleclindz <alexander.v.lindsay@gmail.com>
2017-04-17 17:06:35 -0700

    Initial Commit

---
+++ LiveStream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveStream
+//
+//  Created by Alexander Lindsay on 4/17/17.
+//  Copyright © 2017 com.continuum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
