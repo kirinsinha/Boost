97d49b6b88318fd07cb39a707fb19942886151dc
Kirin Sinha Prior <kirinsinhaprior@DN0a62e27b.SUNet>
2017-05-01 14:17:38 -0700

    logout + full navigation

---
+++ LiveStream/LogoutTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LogoutTableViewCell.swift
+//  LiveStream
+//
+//  Created by Kirin Sinha Prior on 5/1/17.
+//  Copyright © 2017 com.continuum. All rights reserved.
+//
+
+import UIKit
+
+class LogoutTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ LiveStream/ProfileCell.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileCell.swift
+//  LiveStream
+//
+//  Created by Kirin Sinha Prior on 5/1/17.
+//  Copyright © 2017 com.continuum. All rights reserved.
+//
+
+import Foundation
