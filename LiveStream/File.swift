b46d6b39b381abc5db5a2938fba04e35f6d52773
Kirin Sinha Prior <kirinsinhaprior@DN0a62e27b.SUNet>
2017-04-30 22:25:21 -0700

    added login / firebase and some basic navigation

---
+++ LiveStream/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  LiveStream
+//
+//  Created by Kirin Sinha Prior on 4/30/17.
+//  Copyright © 2017 com.continuum. All rights reserved.
+//
+
+import Foundation
---
+++ MenuVC.swift
@@ -0,0 +1,9 @@
+//
+//  MenuVC.swift
+//  
+//
+//  Created by Kirin Sinha Prior on 4/30/17.
+//
+//
+
+import Foundation
